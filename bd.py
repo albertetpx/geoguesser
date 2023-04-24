10d0b4ef6aa0f35784279c0400427ec5ab785256
Albert Guardiola <aguardiola@etpx.fjaverianas.com>
2023-04-24 10:05:46 +0200

    Initial commit

---
+++ bd.py
@@ -0,0 +1,32 @@
+import mysql.connector as mysql
+import random
+
+# Funciones de usuario
+def pedirListaBD():
+    bd = mysql.connect(user="root",password="claumestra",host="127.0.0.1",
+                        database="world")
+    cursor = bd.cursor()
+    query = "select name from country;"
+    cursor.execute(query)
+    listaPaises = cursor.fetchall()
+    bd.close()
+    return listaPaises
+
+def seleccionarPaisRandom():
+    listaPaises = pedirListaBD()            #listaPaises es una lista de tuplas
+    paisRandom = random.choice(listaPaises) #paisRandom es una tupla de una posición
+    return paisRandom[0]
+
+def comprobarCapital(pais,capitalUsuario):
+    bd = mysql.connect(user="root",password="claumestra",host="127.0.0.1",
+                        database="world")
+    cursor = bd.cursor()
+    query = f"select ct.name from country c join city ct on c.capital=ct.id where c.name='{pais}';"
+    cursor.execute(query)
+    capitalOK = cursor.fetchall()[0][0]
+    bd.close()
+    #Comprobamos si la capital es correcta
+    if capitalUsuario == capitalOK:
+        return True, capitalOK
+    else:
+        return False, capitalOK
---
+++ geo.py
@@ -0,0 +1,30 @@
+"""
+Intro a Full Stack Web
+Programa de acertar capitales
+Author: 1r DAW
+"""
+from flask import Flask, render_template, request
+import bd
+
+# Secuencia principal de programa
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    # pais = seleccionarPaisRandom()
+    pais  = "Francia"
+    return render_template("index.html", paisHtml=pais)
+
+@app.route('/checkCapital', methods=["GET", "POST"])
+def checkCapital():
+    if request.method == 'POST':
+        # print(request.form)
+        capitalUsuario = request.form.get("capital")
+        pais= request.form.get("pais")
+        # resultado = comprobarCapitalDirectamenteenWikipedia(pais,capitalUsuario)
+        # resultado, capitalOK = bd.comprobarCapital(pais,capitalUsuario)
+        resultado = True
+        capitalOK = "Capital del país"
+        return render_template("resultado.html", resultadoHtml=resultado, 
+            capitalHtml=capitalOK)
+app.run()
